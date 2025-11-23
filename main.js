c3fa98c5c99e42ff38f6a67ec5d6a091082aeea8
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2025-11-23 09:39:31 +0100

    Implement IPSBoard class with variable management

---
+++ main.js
@@ -0,0 +1,42 @@
+class IPSBoard {
+    constructor() {
+        this.boards = new Map();
+        this.variables = new Map();
+    }
+
+    createBoard(name) {
+        if (!this.boards.has(name)) {
+            this.boards.set(name, new Map());
+        }
+    }
+
+    setVariable(boardName, key, value) {
+        if (!this.boards.has(boardName)) {
+            throw new Error(`Board "${boardName}" does not exist.`);
+        }
+        this.boards.get(boardName).set(key, value);
+    }
+
+    getVariable(boardName, key) {
+        if (!this.boards.has(boardName)) {
+            throw new Error(`Board "${boardName}" does not exist.`);
+        }
+        return this.boards.get(boardName).get(key);
+    }
+
+    createHTML(boardName) {
+        if (!this.boards.has(boardName)) {
+            throw new Error(`Board "${boardName}" does not exist.`);
+        }
+        const board = this.boards.get(boardName);
+        let html = `<div class="ips-board" id="${boardName}">\n`;
+        for (let [key, value] of board.entries()) {
+            html += `  <div class="ips-variable" data-key="${key}">${value}</div>\n`;
+        }
+        html += `</div>`;
+        return html;
+    
+    }
+}
+
+window.IPSBoard = IPSBoard;
